1980039faf1951006952fc3a5a70d283cb458bae
ASWIN-SENTHILKUMAR2006 <aswinsenthilkumarcse@gmail.com>
2026-07-02 15:40:23 +0530

    Spring Core

---
+++ WEEK 03/SPRING CORE/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+/**
+ * @author Aswin Senthilkumar
+ *
+ */
+public class BookRepository {
+	public void displayRepository() {
+        System.out.println("Book Repository is working.");
+    }
+}
---
+++ WEEK 03/SPRING CORE/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,11 @@
+package com.library.service;
+
+/**
+ * @author Aswin Senthilkumar
+ *
+ */
+public class BookService {
+	public void displayService() {
+        System.out.println("Book Service is working.");
+    }
+}
