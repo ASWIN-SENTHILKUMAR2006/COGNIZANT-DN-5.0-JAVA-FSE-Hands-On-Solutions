7a03f47feca1409c2bdd77728e5b102e54a98465
ASWIN-SENTHILKUMAR2006 <aswinsenthilkumarcse@gmail.com>
2026-07-09 17:27:00 +0530

    Spring hands on

--- WEEK 02/MOCKITO/EXERCISE01/src/test/java/cafe/CoffeMakerTest.java
+++ WEEK 02/MOCKITO/EXERCISE01/src/test/java/cafe/CoffeMakerTest.java
@@ -51,7 +51,7 @@ boolean result = coffeeMachine.MakeCoffee();
 //Assert 
 																		//verifying --MOCKITO PHASE -02
 Assertions.assertEquals(true,result,"Coffee was not made -- returned false");
-verify(sugar,times(1)).addSugar();
+verify(sugar,times(1 )).addSugar();
 verify(coffee,times(1)).addCoffee();//never()
 	
 }
